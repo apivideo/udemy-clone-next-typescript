090a1fda8e7dfeac39c93f46ae342522343ccc7d
Aya Bochman <aya@api.video>
2022-04-21 09:45:56 +0200

    Updated content api

--- src/pages/api/content.ts
+++ src/pages/api/content.ts
@@ -4,16 +4,6 @@ const handler = async (req, res) => {
   try {
     const { videos } = req.body
     const result = updateContent(videos)
-    // const appId = process.env.SYMBL_APP_ID;
-    // const appSecret = process.env.SYMBL_APP_SECRET;
-    // const url = 'https://api.symbl.ai/oauth2/token:generate';
-    // const data = {
-    //   type: 'application',
-    //   appId,
-    //   appSecret,
-    // };
-    // const result = await axios.post(url, data);
-    //   const { accessToken } = result.data;
     res.status(200).json({ result });
   }
   catch (err) {
